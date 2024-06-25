6d7feaf9fce4313e961b91b1eba7701a82d539b8
utsav <utsav19october@gmail.com>
2024-06-25 15:57:02 +0530

    improved ui of some screens

---
+++ app/src/main/java/com/example/ichiba/activites/VerifyPhoneActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ichiba.activites
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ichiba.R
+
+class VerifyPhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_verify_phone)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
